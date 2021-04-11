86b79b87c56c6b72cd90cfdafccec823b4f9dd62
Jadilson Domingos <jadilsondomingos86@gmail.com>
2021-04-11 18:17:54 -0300

    sobir

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+  
+ const modificarNome = evento => {
+   console.log(evento.target.value)
+ }
+
+  const criaComboBox = () => {
+    const opcoes= ["cigarro","bebidas"]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option>)
+
+    return(
+         <select>
+           {comboBoxOpcoes}
+         </select>
+    )
+  }
+  
+  
+    
+    const MeuComboBox = ()=> criaComboBox()
+
+
+    return(
+      <>
+      <input type="texte" value={props.nome} onChange={modificarNome}/>
+      <h1>hello {props.nome} sua idade é {props.idade}</h1>
+     
+      <MeuComboBox/>
+      </>
+    )
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App nome="jadlson" idade={31}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
